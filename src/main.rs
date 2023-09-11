2079ab083b697a5ecd17c674f9da387e7866c615
Noor Mohammed B <noormohammedb@protonmail.com>
2023-09-12 00:00:37 +0400

    basic static file server

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
+use actix_files::Files;
 use actix_web::{get, App, HttpServer, Responder};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
   println!("Hello, world!");
-  HttpServer::new(|| App::new().service(index))
-    .bind("0.0.0.0:8080")?
-    .run()
-    .await
+  HttpServer::new(|| {
+    App::new().service(
+      Files::new("/", "./static")
+        .prefer_utf8(true)
+        .index_file("index.html"),
+    )
+  })
+  .bind("0.0.0.0:8080")?
+  .run()
+  .await
 }
 
 #[get("/")]
